cf5a793c2a1305d7fcba1a06c9bd8c7c048d6678
Sergio <popckovM10@yandex.ru>
2022-02-25 07:23:27 +0300

    Наброски настроек

--- settings/names.py
+++ settings/names.py
@@ -1,39 +1,44 @@
+from utils.utils import get_summ, get_var_1, get_var_2
+
 
 # типы документации
 DOCUMENT_TYPES = ('type1', 'type2', 'type3')
+
+TYPES = []
 
 DOCUMENT_TEMPLATE = {
     'type1': {
         'meta': {
             'link': 'registration_number'
         },
-        'data': [
-            {
-                'registration_number': 'Номер связки данных',
+        'data': {
+            'data_1': {
+                'registration_number': 'Номер для связки данных',
                 'area': 'Площадь',
                 'category': 'Категория земель'
             },
-            {
+            'data_2': {
                 'name': 'Имя',
                 'address': 'Адрес',
-                'date': 'Дата'
+                'date': 'Дата',
+                'price': 'Цена'
             },
-        ],
+        },
         'result': {
             'excel': {
+                # отражает собой один документ с несколькими страницами
                 'Документ Excel №1': {
                     'Страница №1': {
-                        'items': {
+                        'items': { # формируется только из данных источника data_2 + кастомные поля
                             'name': {'title': 'Имя', 'type': str()},
                             'address': {'title': 'Адрес', 'type': str()},
                             'date': {'title': 'Дата', 'type': str()},
-                            'area': {'title': 'Площадь', 'type': str()},
-                            'category': {'title': 'Категория земель', 'type': str()},
-                            'registration_number': {'title': 'Номер связки данных', 'type': int()},
+                            'price': {'title': 'Цена', 'type': float()},
+                            'var_1': (get_var_1, 'address', 'date'),
                         }
                     },
                     'Страница №2': {
-                        'items': {
+                        'items': {# формируется из источников data_1 и data_2
                             'name': {'title': 'Имя', 'type': str()},
                             'address': {'title': 'Адрес', 'type': str()},
                             'date': {'title': 'Дата', 'type': str()},
@@ -44,23 +49,26 @@ DOCUMENT_TEMPLATE = {
                     }
                 },
 
+                # отражает собой один документ с несколькими страницами
                 'Документ Excel №2': {
                     'Страница №1': {
-                        'items': {
+                        'items': {# формируется из источников data_1 и data_2 и + кастомные поля
                             'name': {'title': 'Имя', 'type': str()},
                             'address': {'title': 'Адрес', 'type': str()},
                             'date': {'title': 'Дата', 'type': str()},
                             'area': {'title': 'Площадь', 'type': str()},
                             'category': {'title': 'Категория земель', 'type': str()},
                             'registration_number': {'title': 'Номер связки данных', 'type': int()},
-                        }
+                        },
+                        'var_2': (get_var_2, 'registration_number', 'address')
                     }
                 }
             },
 
             'docx': {
+                # отражает собой один документ и все что тут указано это данные для вставки
                 'Документ Docx №1': {
-                    'items_1': {
+                    'items_1': { # формируется только из данных источника date_1 и data_2
                         'name': {'title': 'Имя', 'type': str()},
                         'address': {'title': 'Адрес', 'type': str()},
                         'date': {'title': 'Дата', 'type': str()},
@@ -68,13 +76,13 @@ DOCUMENT_TEMPLATE = {
                         'category': {'title': 'Категория земель', 'type': str()},
                         'registration_number': {'title': 'Номер связки данных', 'type': int()},
                     },
-                    'items_2': {
+                    'items_2': { # формируется только из данных источника data_2
                         'name': {'title': 'Имя', 'type': str()},
                         'address': {'title': 'Адрес', 'type': str()},
+                        'price': {'title': 'Цена', 'type': float()},
                     },
-                    # TODO как сделать генерацию данных что вычисляются из других данных ?
-                    #  а не берутся на прямую
-
+                                # вычисляемое поле, функция для коллбэка и аргументы которые он вычисляет
+                    'summ': (get_summ, 'price')
                 }
             }
         },
---
+++ utils/utils.py
@@ -0,0 +1,13 @@
+
+
+def get_summ(*args):
+    """Возвращает сумму"""
+    return args
+
+
+def get_var_1(*args):
+    return args
+
+
+def get_var_2(*args):
+    return args
